8a253d8e5fcb133afa81028561ce39b259956665
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-28 21:56:33 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,31 +1,43 @@
-from tgbot import tgbot
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from tgbot import tgbot
 
 app = FastAPI()
+
+# Монтируем директорию для статических файлов
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Обработка favicon
 @app.get("/favicon.png")
 async def favicon():
     return RedirectResponse(url="/static/favicon.png")
 
+# Установка webhook при старте
 @app.on_event("startup")
 async def on_startup():
-    print("Setting webhook...")
-    webhook_info = await tgbot.bot.get_webhook_info()
-    if webhook_info.url != tgbot.webhook_url:
-        await tgbot.set_webhook()
-        print(f"Webhook set to {tgbot.webhook_url}")
-    else:
-        print("Webhook already set")
-    
+    try:
+        print("Setting webhook...")
+        webhook_info = await tgbot.bot.get_webhook_info()
+        if webhook_info.url != tgbot.webhook_url:
+            await tgbot.set_webhook()
+            print(f"Webhook set to {tgbot.webhook_url}")
+        else:
+            print("Webhook already set")
+    except Exception as e:
+        print(f"Error setting webhook: {e}")
+
+# Главная страница
 @app.get("/")
 async def read_root():
     return {"message": "Привет, мир!"}
 
-
+# Обработка webhook-запросов от Telegram
 @app.post('/api/bot')
 async def tgbot_webhook_route(request: Request):
-    update_dict = await request.json()
-    await tgbot.update_bot(update_dict)
-    return ''
+    try:
+        update_dict = await request.json()
+        await tgbot.update_bot(update_dict)
+        return ''
+    except Exception as e:
+        return {"error": str(e)}
