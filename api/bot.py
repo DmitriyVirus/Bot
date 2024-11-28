8e68a6a6ff44ee89a23b058e63c320299d16bb77
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-28 21:49:50 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/favicon.png")
 async def favicon():
