d1c2c301c7f990860b784244f3d90700713ad6d9
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-28 21:31:03 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -1,23 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, Router
-from aiogram.types import Update
+from config import config
 from tgbot.handlers import router
-from config import config  # Используем конфигурацию для токена
 
 class TGBot:
     def __init__(self, router: Router) -> None:
-        token = config('TOKEN')
-        self.bot = Bot(token)
+        self.token = config('TOKEN')
+        self.webhook_url = config('WEBHOOK_URL')
+        self.bot = Bot(self.token)
         self.dp = Dispatcher()
         self.dp.include_router(router)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.set_webhook())
 
     async def update_bot(self, update: dict) -> None:
         await self.dp.feed_raw_update(self.bot, update)
-        await self.bot.session.close()
-        
-     async def set_webhook(self) -> None:
+
+    async def set_webhook(self) -> None:
         await self.bot.set_webhook(self.webhook_url)
 
 tgbot = TGBot(router)
