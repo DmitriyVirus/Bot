3d42b6c47654a9711f714457f7c665b8b0d740d5
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-28 16:08:15 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+from decouple import config
+
+TOKEN = config('TOKEN')
+WEBHOOK_URL = config('WEBHOOK_URL')
---
+++ tgbot/__init__.py
@@ -0,0 +1,25 @@
+import asyncio
+from aiogram import Bot, Dispatcher, Router
+from aiogram.types import Update
+from tgbot.handlers import router
+from config import config  # Используем конфигурацию для токена
+
+class TGBot:
+    def __init__(self, router: Router) -> None:
+        token = config('TOKEN')
+        self.bot = Bot(token)
+        self.dp = Dispatcher()
+        self.dp.include_router(router)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.set_webhook())
+
+    async def update_bot(self, update: dict) -> None:
+        await self.dp.feed_raw_update(self.bot, update)
+        await self.bot.session.close()
+
+    async def set_webhook(self):
+        webhook_url = config('WEBHOOK_URL')  # URL вебхука
+        await self.bot.set_webhook(webhook_url)
+        await self.bot.session.close()
+
+tgbot = TGBot(router)
