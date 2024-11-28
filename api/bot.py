c4deba82d14ddd7356d882d17ffd9dd9a2452774
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-28 16:40:20 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,25 +1,8 @@
-from fastapi import FastAPI, Request
-from tgbot.handlers import send_help, welcome_new_user, text_trigger_handler
+from aiogram import Router
+from aiogram.types import Message
 
-app = FastAPI()
+router = Router()
 
-@app.post('/api/bot')
-async def tgbot_webhook_route(request: Request):
-    update = await request.json()  # Получаем обновление от Telegram
-
-    if "message" in update:
-        message = update["message"]
-
-        # Если это новая команда /help
-        if "text" in message and message["text"].startswith("/help"):
-            await send_help(message)
-        
-        # Если это сообщение от нового пользователя
-        elif "new_chat_members" in message:
-            await welcome_new_user(message)
-        
-        # Обработка текстовых сообщений с триггерами
-        elif "text" in message:
-            await text_trigger_handler(message)
-    
-    return ''
+@router.message()
+async def echo(message: Message):
+    await message.answer(f"Echo: {message.text}")
