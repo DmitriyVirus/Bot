648cba2e0e69724be6bae9e4d30240f99e79d067
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-30 00:01:25 +0200

    Update handlers.py

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import Command 
+from aiogram.filters import Command
 from tgbot.triggers import TRIGGERS
 
 router = Router()
@@ -21,20 +21,23 @@ async def greet_new_members(message: Message):
             logging.info(f"Отправлено приветствие для {new_member.first_name} (ID: {new_member.id})")
         except Exception as e:
             logging.error(f"Ошибка при отправке приветствия {new_member.first_name}: {e}")
-            
+
 # Обработчик команды /help
 @router.message(Command(commands=["help"]))  # Используем фильтр Command
 async def help_handler(message: Message):
     help_text = "*Привет, дружище! Я Бот этого чата и слежу за тобой!*\n\n" \
                 "Я приветствую новичков, слежу за порядком и делаю рассылки по активностям.\n\n" \
-                "Так же, я могу ответить на следующие фразы:\n"
-        for i, trigger in enumerate(TRIGGERS, 1): # Перебираем триггеры и нумеруем их
+                "Также я могу ответить на следующие фразы:\n"
+    
+    # Перебираем триггеры и нумеруем их
+    for i, trigger in enumerate(TRIGGERS, 1):
         trigger_text = trigger.split(":")[0]  # Извлекаем часть до символа ":" или оставляем сам текст, если ":" нет
-        trigger_text = trigger_text.capitalize() # Преобразуем первую букву в верхний регистр
+        trigger_text = trigger_text.capitalize()  # Преобразуем первую букву в верхний регистр
         help_text += f"{i}. {trigger_text}\n"  # Добавляем номер и фразу
+    
     await message.answer(help_text, parse_mode="Markdown")
 
-# Обрабатываем Тригеры
+# Обрабатываем Триггеры
 @router.message(lambda message: message.text and any(trigger in message.text.lower() for trigger in TRIGGERS))
 async def trigger_handler(message: Message):
     message_text = message.text.lower()
@@ -42,7 +45,7 @@ async def trigger_handler(message: Message):
         if trigger in message_text:
             if isinstance(response, dict):
                 if "text" in response:
-                    await message.answer(response["text"], parse_mode="Markdown") 
+                    await message.answer(response["text"], parse_mode="Markdown")
                 if "image" in response:
                     try:
                         await message.answer_photo(response["image"])
