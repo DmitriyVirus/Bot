52903d5a6665fd7989e1a8e87f7097de2240d09d
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-29 14:12:42 +0200

    Update handlers.py

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -1,8 +1,27 @@
 from aiogram import Router
 from aiogram.types import Message
+from tgbot.triggers import TRIGGERS
 
 router = Router()
 
 @router.message()
-async def echo(message: Message):
-    await message.answer(f"Повторяю: {message.text}")
+async def trigger_handler(message: Message):
+    message_text = message.text.lower()  # Преобразуем текст в нижний регистр
+    for trigger, response in TRIGGERS.items():
+        if trigger in message_text:
+            if isinstance(response, dict):  # Если ответ это словарь (с текстом, изображением или gif)
+                # Отправляем текст
+                if "text" in response:
+                    await message.answer(response["text"])
+                
+                # Отправляем изображение, если есть
+                if "image" in response:
+                    await message.answer_photo(response["image"])
+                
+                # Отправляем gif, если есть
+                if "gif" in response:
+                    await message.answer_animation(response["gif"])
+            
+            else:
+                await message.answer(response)  # Отправляем текст
+            break  # Прекращаем проверку после первого совпадения
