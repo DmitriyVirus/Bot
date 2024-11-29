9aade249ef55aa8be9ad2a95ae2f31a0984f1a95
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-29 14:25:34 +0200

    Update handlers.py

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -4,6 +4,16 @@ from tgbot.triggers import TRIGGERS
 
 router = Router()
 
+# Обработчик команды /help
+@router.message(commands=["help"])
+async def help_handler(message: Message):
+    help_text = (
+        "Я приветствую новиков и реагирую на некоторые фразы:\n\n"
+        "Я могу ответить на следующие фразы:\n"
+        f"{''.join(f'*{trigger}* - {response if isinstance(response, str) else response.get('text', '')}\n' for trigger, response in TRIGGERS.items())}"
+    )
+    await message.answer(help_text, parse_mode="Markdown")
+
 @router.message()
 async def trigger_handler(message: Message):
     message_text = message.text.lower()  # Преобразуем текст в нижний регистр
