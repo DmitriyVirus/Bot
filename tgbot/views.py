bf058535a128821396ac4f20214e6cb64ef13e3f
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-30 23:59:50 +0200

    Delete tgbot/views.py

--- tgbot/views.py
+++
@@ -1,6 +0,0 @@
-# tgbot/views.py
-def join_message(username):
-    return f"Привет, {username}! Добро пожаловать в наш чат!"
-
-def left_message(username):
-    return f"Прощай, {username}. Надеемся, ты вернешься!"
