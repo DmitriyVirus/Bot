acb9694a67c68bea099829e9d0351f0895a1e50f
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-11-30 23:49:36 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,6 +1,8 @@
 import os
 import json
+import random
 import logging
+import datetime
 from tgbot import tgbot
 from decouple import config
 from fastapi import FastAPI, Request
@@ -50,14 +52,54 @@ async def tgbot_webhook_route(request: Request):
 async def favicon():
     return FileResponse(os.path.join(os.getcwd(), "static", "favicon.ico"))
 
-
 @app.get('/send_reminder', include_in_schema=False)
 async def send_reminder():
     try:
-        text = "Привет! Это ваше напоминание на будний день."
-        photo_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Blue_sky%2C_white-gray_clouds.JPG/1920px-Blue_sky%2C_white-gray_clouds.JPG"
-        # Отправка текста
+        # Получаем текущий день недели
+        day_of_week = datetime.datetime.now().weekday()
+        
+        # Логика для дней недели
+        if day_of_week in [0]:  # Понедельник
+            text = "Утро добрым не бывает, а понедельник ведь все-таки день тяжелый... Но не унываем!"  
+            file_path = os.path.join(os.getcwd(), "static", "mond_url.txt")
+        elif day_of_week in [1, 2, 3]:  # Вторник, Среда, Четверг
+            text = "Всем доброго утра! Рабочий день начинается!"
+            file_path = os.path.join(os.getcwd(), "static", "workdays_url.txt")
+        elif day_of_week in [4]:  # Пятница
+            text = "Всем доброго утра! А вот вы знали, что сегодня пятница?!"
+            file_path = os.path.join(os.getcwd(), "static", "fri_url.txt")
+        elif day_of_week in [5, 6]:  # Выходные
+            text = "Всем доброго утра! Выхходные! Гуляеммм!!!"
+            file_path = os.path.join(os.getcwd(), "static", "weekend_url.txt")
+        else:
+            return {"status": "skipped", "message": "Not a reminder day"}
+        # Загрузка ссылок из файла
+        with open(file_path, "r") as file:
+            photo_urls = file.readlines()
+        # Выбор случайной ссылки
+        photo_url = random.choice(photo_urls).strip()  # Убираем лишние пробелы или символы новой строки
+        # Отправка текста и фото
         await tgbot.bot.send_photo(chat_id=config('CHAT_ID'), photo=photo_url, caption=text)
+        return {"status": "success", "message": "Reminder sent"}
+    
+    except Exception as e:
+        logging.error(f"Ошибка при отправке напоминания: {e}")
+        return {"status": "error", "message": str(e)}
+
+@app.get('/send_reminder1', include_in_schema=False)
+async def send_reminder():
+    try:
+        # Получаем текущий день недели (0 - понедельник, 1 - вторник, ..., 6 - воскресенье)
+        day_of_week = datetime.datetime.now().weekday()
+        # Проверяем, что день недели - понедельник (0), вторник (1), среда (2) или четверг (3)
+        if day_of_week in [0, 1, 2, 3, 4]:
+            text = "Значится идем на инсты как обычно в 19:30. Иду Я, Тор и еще кто-то. Ставим + в ответ на мое сообщение, чтобы я видел кто будет участвовать. Если не вижу, то пните меня."
+            photo_url = "https://battleclub.space/uploads/monthly_2022_07/baylor.jpg.02e0df864753bf47b1ef76303b993a1d.jpg"
+            # Отправка текста и фото
+            await tgbot.bot.send_photo(chat_id=config('CHAT_ID'), photo=photo_url, caption=text)
+            return {"status": "success", "message": "Reminder sent"}
+        else:
+            return {"status": "skipped", "message": "Not a reminder day"}
     except Exception as e:
         logging.error(f"Ошибка при отправке напоминания: {e}")
         return {"status": "error", "message": str(e)}
