df94380d1886a5d59b7ca8d78c6eaa50fefb1660
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-01 22:54:26 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,13 +1,11 @@
 import os
 import json
-import random
-import logging
-import datetime
 from tgbot import tgbot
 from decouple import config
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from api.reminder import send_reminder, send_reminder1  # Импортируем обе функции
 
 app = FastAPI()
 
@@ -46,41 +44,12 @@ async def tgbot_webhook_route(request: Request):
         print(f"Error processing update: {e}")
         return {"error": str(e)}
 
-# Обработка favicon
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse(os.path.join(os.getcwd(), "static", "favicon.ico"))
-
+# Вызов функции отправки первого напоминания
 @app.get('/send_reminder', include_in_schema=False)
-async def send_reminder():
-    try:
-        # Получаем текущий день недели
-        day_of_week = datetime.datetime.now().weekday()
-        
-        # Логика для дней недели
-        if day_of_week in [0]:  # Понедельник
-            text = "Утро добрым не бывает, а понедельник ведь все-таки день тяжелый... Но не унываем!"  
-            file_path = os.path.join(os.getcwd(), "urls", "mond_url.txt")
-        elif day_of_week in [1, 2, 3]:  # Вторник, Среда, Четверг
-            text = "Всем доброго утра! Рабочий день начинается!"
-            file_path = os.path.join(os.getcwd(), "urls", "workdays_url.txt")
-        elif day_of_week in [4]:  # Пятница
-            text = "Всем доброго утра! А вот вы знали, что сегодня пятница?!"
-            file_path = os.path.join(os.getcwd(), "urls", "fri_url.txt")
-        elif day_of_week in [5, 6]:  # Выходные
-            text = "Всем доброго утра! Выхходные! Гуляеммм!!!"
-            file_path = os.path.join(os.getcwd(), "urls", "weekend_url.txt")
-        else:
-            return {"status": "skipped", "message": "Not a reminder day"}
-        # Загрузка ссылок из файла
-        with open(file_path, "r") as file:
-            photo_urls = file.readlines()
-        # Выбор случайной ссылки
-        photo_url = random.choice(photo_urls).strip()  # Убираем лишние пробелы или символы новой строки
-        # Отправка текста и фото
-        await tgbot.bot.send_photo(chat_id=config('CHAT_ID'), photo=photo_url, caption=text)
-        return {"status": "success", "message": "Reminder sent"}
-    
-    except Exception as e:
-        logging.error(f"Ошибка при отправке напоминания: {e}")
-        return {"status": "error", "message": str(e)}
+async def send_reminder_route():
+    return await send_reminder()  # Используем функцию из reminder.py
+
+# Вызов функции отправки второго напоминания
+@app.get('/send_reminder1', include_in_schema=False)
+async def send_reminder1_route():
+    return await send_reminder1()  # Используем новую функцию из reminder.py
