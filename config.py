3f0ffb3ce9e1212aac4a72a77600734d70d0d5dd
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-02 20:11:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from decouple import config
 
-TOKEN = config('TOKEN')
-WEBHOOK_URL = config('WEBHOOK_URL')
-CHAT_ID = config('CHAT_ID')
-REDIS_URL = config('REDIS_URL')
+# Основные переменные
+TOKEN = config("TOKEN")
+WEBHOOK_URL = config("WEBHOOK_URL")
+CHAT_ID = config("CHAT_ID")
+REDIS_URL = config("REDIS_URL")
