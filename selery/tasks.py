119af8b19097d3b89a21132f905d9ed79c22cc49
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-02 21:45:33 +0200

    Delete selery directory

--- selery/tasks.py
+++
@@ -1,7 +0,0 @@
-from .celery_config import celery_app
-import time
-
-@celery_app.task(name="tasks.long_task")
-def long_task(duration):
-    time.sleep(duration)
-    return f"Task completed in {duration} seconds"
