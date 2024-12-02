ec71c7af009b674d1b8efb488cdcb906995d5d39
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-02 18:17:44 +0200

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -1,16 +1,12 @@
+from decouple import config
 from celery import Celery
-from upstash_redis import Redis
 
-# Настройка Redis для Upstash
-redis = Redis(url="https://gusc1-striking-platypus-30618.upstash.io", token="AXeaASQgYjdkNWNjOTktMDU3ZS00YzBkLWE2ZGEtNWFkNTg5ZmNlN2M3OWU4ZWE3NTY2NWJkNGVjOWExNTFmZTYyNWQ4NWQxNjg=")
+# Получаем URL для Redis из переменной окружения
+redis_url = config('REDIS_URL')  # Переменная окружения должна быть настроена на вашем сервере
 
-# Инициализация Celery с использованием Redis как брокера
+# Инициализация Celery с использованием Redis как брокера и бэкэнда
 celery_app = Celery(
     "tgbot",
-    broker=f"redis://{redis.client.host}:{redis.client.port}/{redis.client.db}",
-    backend=f"redis://{redis.client.host}:{redis.client.port}/{redis.client.db}",
-)
-
-celery_app.conf.update(
-    result_expires=3600,  # Время хранения результатов задач в секундах
+    broker=redis_url,
+    backend=redis_url,
 )
