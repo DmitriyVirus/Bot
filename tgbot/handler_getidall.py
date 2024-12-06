9c7af8c0c4a0d54c6ce4140b541d1bbf5cb6be0e
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-06 14:19:08 +0200

    Update handler_getidall.py

--- tgbot/handler_getidall.py
+++ tgbot/handler_getidall.py
@@ -11,6 +11,25 @@ router = Router()
 CHAT_ID = -1002388880478  # Замените на ID вашего чата
 PINNED_MESSAGE_ID = 2743  # Замените на ID закрепленного сообщения
 
+# Хендлер для команды /getidbot
+@router.message(Command(commands=["getidbot"]))
+async def send_message_with_id(message: types.Message):
+    try:
+        # Отправляем начальное сообщение
+        sent_message = await message.answer("Получаю id.")
+        
+        # Извлекаем ID отправленного сообщения
+        message_id = sent_message.message_id
+        
+        # Редактируем это сообщение, добавляя его ID в текст
+        updated_text = f"ID этого сообщения: {message_id}"
+        await sent_message.edit_text(updated_text)
+        
+        logging.info(f"Текст обновлен. ID сообщения: {message_id}")  # Логируем ID сообщения
+    except Exception as e:
+        logging.error(f"Ошибка при отправке или редактировании сообщения: {e}")
+        await message.answer("Произошла ошибка. Попробуйте снова.")
+        
 @router.message()
 async def update_message_text(message: types.Message):
     """Обновляет текст закрепленного сообщения с id и именами пользователей, не добавляя повторений"""
