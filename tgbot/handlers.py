cff2a82756edf398b644026b6d00f9aaa14300e9
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-06 20:44:06 +0200

    Update handlers.py

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Router, types
 from aiogram.types import Message
 from aiogram.filters import Command
-from tgbot.triggers import TRIGGERS, WELCOME_TEXT, HELP_TEXT_HEADER, COMMANDS_LIST, NAME_TABLE
+from tgbot.triggers import TRIGGERS, WELCOME_TEXT, HELP_TEXT_HEADER, COMMANDS_LIST, NAME_TABLE, ALIASES
 
 router = Router()
 
