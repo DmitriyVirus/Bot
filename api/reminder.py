3f2fa288090898b31b764189e2bcf2ad809328ed
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-06 23:23:34 +0200

    Update reminder.py

--- api/reminder.py
+++ api/reminder.py
@@ -50,21 +50,22 @@ async def send_reminder1():
         day_of_week = datetime.datetime.now().weekday()
         # Проверяем, что день недели - понедельник (0), вторник (1), среда (2) или четверг (3)
         if day_of_week in [0, 1, 2, 3, 4]:
-            # Создаем message, чтобы передать его в диспетчер
+            # Создаем команду
+            text = "/inst 19:30"
             message = types.Message(
                 message_id=1234,  # Поставьте подходящий id
                 from_user=types.User(id=12345, is_bot=False, first_name="Bot", last_name="Botov", username="botov_user"),  # Создание пользователя
                 chat=types.Chat(id=config('CHAT_ID'), type='private'),  # ID чата
                 date=datetime.datetime.now(),
-                text="/inst 19:30"  # Текст с командой
+                text=text  # Текст с командой
             )
 
-            # Вызов хендлера с использованием диспетчера
-            await tgbot.dp.process_update(types.Update(message=message))  # Обработка update с использованием dispatcher
+            # Передаем обновление в диспетчер
+            update = types.Update(message=message)
+            await tgbot.dp.feed_update(update)  # Обрабатываем обновление через Dispatcher
             return {"status": "success", "message": "Reminder sent"}
         else:
             return {"status": "skipped", "message": "Not a reminder day"}
     except Exception as e:
         logging.error(f"Ошибка при отправке напоминания: {e}")
         return {"status": "error", "message": str(e)}
-
