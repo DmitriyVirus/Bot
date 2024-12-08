cc40864e2a8fe66308c4433ce48d2edbf2447e44
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-08 22:09:06 +0200

    Update handler_sbor.py

--- tgbot/handler_sbor.py
+++ tgbot/handler_sbor.py
@@ -12,7 +12,7 @@ router = Router()
 
 # Хендлер для команды /inst
 @router.message(Command(commands=["inst"]))
-async def fix_handler(message: types.Message):
+async def fix_handler(message: types.Message, bot: Bot):
     try:
         time_match = re.search(r"(\d{1,2}:\d{2})", message.text)
         time = time_match.group(1) if time_match else "когда соберемся"
