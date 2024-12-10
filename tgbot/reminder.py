9ce5d04f4a0b1bd372b94b275013ffea6e1587bd
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-10 19:43:20 +0200

    Update reminder.py

--- tgbot/reminder.py
+++ tgbot/reminder.py
@@ -15,36 +15,12 @@ router = Router()
 @router.message(Command(commands=["goodmornigeverydayGG"]))
 async def good_mornig_every_day_GG(message: types.Message):
     try:
-        # Получаем текущий день недели
-        day_of_week = datetime.datetime.now().weekday()
-        
-        # Логика для дней недели
-        if day_of_week == 0:  # Понедельник
-            text = "Утро добрым не бывает, а понедельник ведь все-таки день тяжелый... Но не унываем!"
-            file_path = os.path.join(os.getcwd(), "urls", "mond_url.txt")
-        elif day_of_week in [1, 2, 3]:  # Вторник, Среда, Четверг
-            text = "Всем доброго утра! Рабочий день начинается!"
-            file_path = os.path.join(os.getcwd(), "urls", "workdays_url.txt")
-        elif day_of_week == 4:  # Пятница
-            text = "Всем доброго утра! А вот вы знали, что сегодня пятница?!"
-            file_path = os.path.join(os.getcwd(), "urls", "fri_url.txt")
-        elif day_of_week in [5, 6]:  # Выходные
-            text = "Всем доброго утра! Выхходные! Гуляеммм!!!"
-            file_path = os.path.join(os.getcwd(), "urls", "weekend_url.txt")
-        else:
-            return {"status": "skipped", "message": "Not a reminder day"}
-
-        # Проверка существования файла
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"Файл {file_path} не найден.")
-
+        text = "Всем доброго утра! Рабочий день начинается!"
+        file_path = os.path.join(os.getcwd(), "urls", "mond_url.txt")
+    
         # Загрузка ссылок из файла
         with open(file_path, "r") as file:
             photo_urls = file.readlines()
-
-        # Проверка, что список не пуст
-        if not photo_urls:
-            raise ValueError(f"Файл {file_path} пуст.")
 
         # Выбор случайной ссылки
         photo_url = random.choice(photo_urls).strip()  # Убираем лишние пробелы или символы новой строки
