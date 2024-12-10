a712a52700b82dfe1c38a7d1322b7c575ae2b60b
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-10 15:38:52 +0200

    Update handler_getidall.py

--- tgbot/handler_getidall.py
+++ tgbot/handler_getidall.py
@@ -11,6 +11,27 @@ router = Router()
 CHAT_ID = -1002388880478  # Замените на ID вашего чата
 PINNED_MESSAGE_ID = 3665  # Замените на ID закрепленного сообщения
 
+@router.message(Command(commands=["getidbot"]))
+async def send_message_with_id(message: types.Message):
+    try:
+        # Указываем ID пользователя, который может вызвать команду
+        allowed_user_id = 559273200  # Замените на нужный ID
+
+        # Проверяем, является ли ID пользователя допустимым
+        if message.from_user.id != allowed_user_id:
+            # Если это не тот пользователь, игнорируем команду или отправляем сообщение
+            return
+        # Если это тот пользователь, выполняем команду
+        sent_message = await message.answer("Получаю id.")
+        message_id = sent_message.message_id
+        updated_text = f"ID этого сообщения: {message_id}"
+        await sent_message.edit_text(updated_text)
+        logging.info(f"Текст обновлен. ID сообщения: {message_id}")
+
+    except Exception as e:
+        logging.error(f"Ошибка при отправке или редактировании сообщения: {e}")
+        await message.answer("Произошла ошибка. Попробуйте снова.")
+
 @router.message()
 async def update_message_text(message: types.Message):
     """Обновляет текст закрепленного сообщения с id и именами пользователей, не добавляя повторений"""
@@ -53,24 +74,3 @@ async def update_message_text(message: types.Message):
 
     except Exception as e:
         logging.error(f"Ошибка при обновлении сообщения: {type(e).__name__}: {e}")
-
-@router.message(Command(commands=["getidbot"]))
-async def send_message_with_id(message: types.Message):
-    try:
-        # Указываем ID пользователя, который может вызвать команду
-        allowed_user_id = 559273200  # Замените на нужный ID
-
-        # Проверяем, является ли ID пользователя допустимым
-        if message.from_user.id != allowed_user_id:
-            # Если это не тот пользователь, игнорируем команду или отправляем сообщение
-            return
-        # Если это тот пользователь, выполняем команду
-        sent_message = await message.answer("Получаю id.")
-        message_id = sent_message.message_id
-        updated_text = f"ID этого сообщения: {message_id}"
-        await sent_message.edit_text(updated_text)
-        logging.info(f"Текст обновлен. ID сообщения: {message_id}")
-
-    except Exception as e:
-        logging.error(f"Ошибка при отправке или редактировании сообщения: {e}")
-        await message.answer("Произошла ошибка. Попробуйте снова.")
