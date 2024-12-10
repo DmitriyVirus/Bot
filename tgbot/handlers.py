78836a8f9802d763ca2581ab84aabfe0dd936dc8
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-10 16:45:30 +0200

    Update handlers.py

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -1,7 +1,11 @@
 import logging
+import datetime
+from decouple import config
 from aiogram import Router, types
 from aiogram.types import Message
 from aiogram.filters import Command
+from fastapi import FastAPI, Request, HTTPException
+from aiogram.types import Message, User, Chat, InlineKeyboardButton, InlineKeyboardMarkup
 from tgbot.triggers import TRIGGERS, WELCOME_TEXT, HELP_TEXT_HEADER, COMMANDS_LIST, NAME_TABLE, ALIASES
 
 router = Router()
@@ -225,3 +229,41 @@ async def trigger_handler(message: Message):
             else:
                 await message.answer(response, parse_mode="Markdown")
             break
+
+@router.message(Command(commands=["goodmornigeverydayGG"]))
+async def good_mornig_every_day_GG():
+    try:
+        # Получаем текущий день недели
+        day_of_week = datetime.datetime.now().weekday()
+        
+        # Логика для дней недели
+        if day_of_week == 0:  # Понедельник
+            text = "Утро добрым не бывает, а понедельник ведь все-таки день тяжелый... Но не унываем!"
+            file_path = os.path.join(os.getcwd(), "urls", "mond_url.txt")
+        elif day_of_week in [1, 2, 3]:  # Вторник, Среда, Четверг
+            text = "Всем доброго утра! Рабочий день начинается!"
+            file_path = os.path.join(os.getcwd(), "urls", "workdays_url.txt")
+        elif day_of_week == 4:  # Пятница
+            text = "Всем доброго утра! А вот вы знали, что сегодня пятница?!"
+            file_path = os.path.join(os.getcwd(), "urls", "fri_url.txt")
+        elif day_of_week in [5, 6]:  # Выходные
+            text = "Всем доброго утра! Выхходные! Гуляеммм!!!"
+            file_path = os.path.join(os.getcwd(), "urls", "weekend_url.txt")
+        else:
+            return {"status": "skipped", "message": "Not a reminder day"}
+
+        # Загрузка ссылок из файла
+        with open(file_path, "r") as file:
+            photo_urls = file.readlines()
+
+        # Выбор случайной ссылки
+        photo_url = random.choice(photo_urls).strip()  # Убираем лишние пробелы или символы новой строки
+        
+        # Отправка текста и фото
+        await tgbot.bot.send_photo(chat_id=config('CHAT_ID'), photo=photo_url, caption=text)
+        return {"status": "success", "message": "Reminder sent"}
+
+    except Exception as e:
+        logging.error(f"Ошибка при отправке напоминания: {e}")
+        return {"status": "error", "message": str(e)}
+
