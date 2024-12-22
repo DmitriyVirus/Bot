6f650101118d5b3225ebfc5ffef065754d1927e6
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2024-12-22 16:42:11 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, Router
 from tgbot.handlers import router as handlers_router # Убедитесь, что импортируете router из handlers
 from tgbot.handler_sbor import router as handler_sbor_router
 from tgbot.handler_getidall import router as handler_getidall_router
+from tgbot.google_sheets import add_user_to_sheet  # Импорт функции добавления пользователя
 
 router = Router()
 router.include_router(handlers_router)  # Подключаем хендлеры
