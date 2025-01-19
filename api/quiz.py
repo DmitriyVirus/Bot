dadee521987552f8667e26252033b8b75d184594
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2025-01-19 22:09:37 +0200

    Delete api/quiz.py

--- api/quiz.py
+++
@@ -1,7 +0,0 @@
-import os
-from tgbot.gspread_client import get_gspread_client  # Импортируем функцию из gspread_client
-
-# Функция для сохранения данных пользователя в Google Sheets
-def save_user_data(client, name, difficulty):
-    sheet = client.open("quiz").get_worksheet(1)  # Второй лист
-    sheet.append_row([name, difficulty])
