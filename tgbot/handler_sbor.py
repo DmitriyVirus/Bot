9a75f07ac5d85016478a33bf3878118a11f0b0da
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-01-31 19:19:46 +0200

    Update handler_sbor.py

--- tgbot/handler_sbor.py
+++ tgbot/handler_sbor.py
@@ -33,7 +33,7 @@ async def bal_handler(message: types.Message):
         time_match = re.search(r"(\d{1,2}:\d{2}(?:-\d{1,2}:\d{2})?)", message.text)
         time = time_match.group(1) if time_match else "когда соберемся"
 
-        photo_url = "https://i.pinimg.com/736x/c1/9f/e0/c19fe044879586dbeb6ea8fbd947456f.jpg"
+        photo_url = "https://i.pinimg.com/736x/ba/6c/7c/ba6c7c9c1bbde89410e5bcd8736166b2.jpg"
         keyboard = create_keyboard()
 
         caption = (
