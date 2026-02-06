3c485733032cce65555b206979f811e0c239c59c
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 14:57:34 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,1 +1,9 @@
 # Папка handlers является пакетом
+# tgbot/handlers/__init__.py
+
+from .menu import router as menu_router
+from .triggers import router as triggers_router
+from .greetings import router as greetings_router
+
+# Собираем все роутеры в один список
+routers = [menu_router, triggers_router, greetings_router]
