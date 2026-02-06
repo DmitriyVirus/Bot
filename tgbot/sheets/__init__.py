a5222d31b8c4cbacd84bbad5149106dd62cad716
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 16:39:15 +0200

    Create __init__.py

---
+++ tgbot/sheets/__init__.py
@@ -0,0 +1,7 @@
+# Импортируем все роутеры из отдельных файлов
+from .autoget import router as autoget_router
+from aiogram import Router
+
+# Создаём единый роутер для всех хэндлеров
+router = Router()
+router.include_router(autoget_router)
