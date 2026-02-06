6181df1424007cb8a8ebb142414349a9cc055163
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 18:46:16 +0200

    Update __init__.py

--- tgbot/sheets/__init__.py
+++ tgbot/sheets/__init__.py
@@ -1,8 +1,11 @@
 # Импортируем все роутеры из отдельных файлов
 from .autoget import router as autoget_router
+from .adminka import router as adminka_router
 from .autoget import add_user_to_sheet
 from aiogram import Router
 
 # Создаём единый роутер для всех хэндлеров
 router = Router()
 router.include_router(autoget_router)
+router.include_router(adminka_router)
+
