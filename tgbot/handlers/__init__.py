62606e0c2c86fff4c8edef5f36bed94fc19a18a7
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 19:46:15 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,6 +1,7 @@
 # Импортируем все роутеры из отдельных файлов
 from .menu import router as menu_router
 from .kto import router as kto_router
+from .getid import router as getid_router
 from .triggers import router as triggers_router
 from .greetings import router as greetings_router
 from .kto import fetch_data_from_sheet
@@ -10,5 +11,6 @@ from aiogram import Router
 router = Router()
 router.include_router(kto_router)
 router.include_router(menu_router)
+router.include_router(getid_router)
 router.include_router(triggers_router)
 router.include_router(greetings_router)
