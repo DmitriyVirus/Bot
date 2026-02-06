15c99d8763c6850ae4451c04ed6614f4bc2907e2
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 17:55:22 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -1,36 +1,31 @@
 import os
-from config import config
+import asyncio
+from config import config  # Используем конфигурацию для токена
 from aiogram import Bot, Dispatcher, Router
-
-# Подключаем роутеры
-from tgbot.handlers import router as handlers_router
+from tgbot.handlers import router as handlers_router # Убедитесь, что импортируете router из handlers
 from tgbot.handler_sbor import router as handler_sbor_router
+from tgbot.handler_getidall import router as handler_getidall_router
+from tgbot.google_sheets import router as google_sheets_router  # Импорт функции добавления пользователя
+from tgbot.google_sheets import add_user_to_sheet, fetch_data_from_sheet  # Импорт функции добавления пользователя
 from tgbot.gspread_client import get_gspread_client
 from tgbot.google_tab import router as google_tab_router
-from tgbot.sheets import router as sheets_router  # автодобавление
-from tgbot.handler_getidall import router as handler_getidall_router
-from tgbot.google_sheets import router as google_sheets_router # Новый роутер с /backup 
-from tgbot.google_sheets import add_user_to_sheet, fetch_data_from_sheet # Импорт функций работы с Google Sheets
 
-# Главный роутер
+
 router = Router()
-router.include_router(sheets_router)          # автодобавление пользователей
 router.include_router(google_tab_router)
-router.include_router(handlers_router)
+router.include_router(handlers_router)  # Подключаем хендлеры
 router.include_router(handler_sbor_router)
 router.include_router(handler_getidall_router)
 router.include_router(google_sheets_router)
 
-# Класс бота
+
+
 class TGBot:
     def __init__(self, router: Router) -> None:
         token = os.getenv('TOKEN')
-        if not token:
-            raise RuntimeError("TOKEN is not set in environment variables")
-
         self.bot = Bot(token)
         self.dp = Dispatcher()
-        self.dp.include_router(router)  # подключаем главный роутер
+        self.dp.include_router(router)  # Подключаем роутер
         self.webhook_url = config('WEBHOOK_URL')
 
     async def update_bot(self, update: dict) -> None:
@@ -41,9 +36,8 @@ class TGBot:
         await self.bot.set_webhook(self.webhook_url)
         print(f"Webhook set to {self.webhook_url}")
 
-# Инициализация бота
+# Инициализация tgbot с импортированным router
 tgbot = TGBot(router)
-
 
 
 
