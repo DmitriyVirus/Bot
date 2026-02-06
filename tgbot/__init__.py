ea6140975b266eb2749f98cd1016cf22f7db2a3f
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-06 15:12:02 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -1,27 +1,26 @@
 import os
 import asyncio
-from config import config
+from config import config  # Используем переменные окружения
 from aiogram import Bot, Dispatcher, Router
 
-from tgbot.handlers import routers  # Список роутеров: menu_router, greetings_router, triggers_router
+# Подключаем единый роутер стандартных хэндлеров
+from tgbot.handlers import router as handlers_router
 from tgbot.handler_sbor import router as handler_sbor_router
 from tgbot.handler_getidall import router as handler_getidall_router
 from tgbot.google_sheets import router as google_sheets_router
+from tgbot.google_sheets import add_user_to_sheet, fetch_data_from_sheet
+from tgbot.gspread_client import get_gspread_client
 from tgbot.google_tab import router as google_tab_router
 
 # Главный роутер
 router = Router()
 
-# Подключаем роутеры
+# Подключаем все роутеры
 router.include_router(google_tab_router)
+router.include_router(handlers_router)        # Подключаем все стандартные хэндлеры
 router.include_router(handler_sbor_router)
 router.include_router(google_sheets_router)
 router.include_router(handler_getidall_router)
-
-# Подключаем стандартные роутеры из handlers
-for r in routers:
-    router.include_router(r)
-
 
 # Класс бота
 class TGBot:
@@ -29,7 +28,7 @@ class TGBot:
         token = os.getenv('TOKEN')
         self.bot = Bot(token)
         self.dp = Dispatcher()
-        self.dp.include_router(router)
+        self.dp.include_router(router)  # Подключаем главный роутер
         self.webhook_url = config('WEBHOOK_URL')
 
     async def update_bot(self, update: dict) -> None:
@@ -40,6 +39,5 @@ class TGBot:
         await self.bot.set_webhook(self.webhook_url)
         print(f"Webhook set to {self.webhook_url}")
 
-
-# Инициализация tgbot с импортированным router
+# Инициализация бота
 tgbot = TGBot(router)
