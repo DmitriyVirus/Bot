0e7ec29b008485c9c3c3158c192fc773352f35d9
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-09 13:16:23 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -1,21 +1,28 @@
 import os
 import asyncio
-from config import config  # Используем конфигурацию для токена
 from aiogram import Bot, Dispatcher, Router
-from tgbot.handlers import router as handlers_router # Убедитесь, что импортируете router из handlers
-from tgbot.sheets import router as sheets_router # Убедитесь, что импортируете router из 
+from tgbot.handlers import router as handlers_router
+from tgbot.sheets import router as sheets_router
 
 router = Router()
-router.include_router(handlers_router)  # Подключаем хендлеры
-router.include_router(sheets_router)  # Подключаем хендлеры
+router.include_router(handlers_router)
+router.include_router(sheets_router)
+
 
 class TGBot:
     def __init__(self, router: Router) -> None:
-        token = os.getenv('TOKEN')
+        token = os.getenv("TOKEN")
+        webhook_url = os.getenv("WEBHOOK_URL")
+
+        if not token:
+            raise RuntimeError("TOKEN не задан в переменных окружения")
+        if not webhook_url:
+            raise RuntimeError("WEBHOOK_URL не задан в переменных окружения")
+
         self.bot = Bot(token)
         self.dp = Dispatcher()
-        self.dp.include_router(router)  # Подключаем роутер
-        self.webhook_url = config('WEBHOOK_URL')
+        self.dp.include_router(router)
+        self.webhook_url = webhook_url
 
     async def update_bot(self, update: dict) -> None:
         await self.dp.feed_raw_update(self.bot, update)
@@ -25,14 +32,5 @@ class TGBot:
         await self.bot.set_webhook(self.webhook_url)
         print(f"Webhook set to {self.webhook_url}")
 
-# Инициализация tgbot с импортированным router
+
 tgbot = TGBot(router)
-
-
-
-
-
-
-
-
-
