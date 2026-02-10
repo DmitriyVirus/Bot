20827e32d5be419bdbb36ef1d9b84664af8a950e
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-10 17:26:24 +0200

    Update __init__.py

--- tgbot/sheets/__init__.py
+++ tgbot/sheets/__init__.py
@@ -1,26 +1,19 @@
-# ===========================
-# tgbot/sheets/__init__.py
-# ===========================
-
-from aiogram import Router
-
 # Импортируем все роутеры из отдельных файлов
 from .autoget import router as autoget_router
-from .autoget import add_user_to_sheet
 from .gspread_client import get_gspread_client
+from .autoget import add_user_to_sheet
+from aiogram import Router
 
 # Импортируем функции для работы с Google Sheets
 from .take_from_sheet import (
-    get_welcome_text,
-    get_hello_text,
-    get_about_bot_text,
+    get_info_column_by_header,
     get_bot_commands,
     get_bot_deb_cmd,
     fetch_participants,
-    get_image_from_cell,
     get_admins_records
 )
 
 # Создаём единый роутер для всех хэндлеров
 router = Router()
 router.include_router(autoget_router)
+
