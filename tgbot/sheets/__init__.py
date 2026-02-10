e586b34852be2eea3a451c7d20b080ae35148fd9
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-10 17:21:35 +0200

    Update __init__.py

--- tgbot/sheets/__init__.py
+++ tgbot/sheets/__init__.py
@@ -1,17 +1,22 @@
+# ===========================
+# tgbot/sheets/__init__.py
+# ===========================
+
+from aiogram import Router
+
 # Импортируем все роутеры из отдельных файлов
 from .autoget import router as autoget_router
-from .gspread_client import get_gspread_client
 from .autoget import add_user_to_sheet
-from aiogram import Router
+from .gspread_client import get_gspread_client
 
 # Импортируем функции для работы с Google Sheets
 from .take_from_sheet import (
-    get_bot_commands,
-    get_bot_deb_cmd,
-    fetch_participants,
     get_welcome_text,
     get_hello_text,
     get_about_bot_text,
+    get_bot_commands,
+    get_bot_deb_cmd,
+    fetch_participants,
     get_image_from_cell,
     get_admins_records
 )
