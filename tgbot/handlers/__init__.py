3a8fa9e7a9bcaf276bbfb3d546b303b84316d57c
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-10 14:36:23 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,6 +1,7 @@
 # Импортируем все роутеры из отдельных файлов
 from .menu import router as menu_router
 from .kto import router as kto_router
+from .adminka import router as adminka_router
 from .getid import router as getid_router
 from .triggers import router as triggers_router
 from .greetings import router as greetings_router
@@ -16,3 +17,4 @@ router.include_router(getid_router)
 router.include_router(sbor_router)
 router.include_router(triggers_router)
 router.include_router(greetings_router)
+router.include_router(adminka_router)
