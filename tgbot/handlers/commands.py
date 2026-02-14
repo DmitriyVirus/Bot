e1a85b4e3331e3288a8d97f0f6d88c2eb6f68623
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-14 20:21:56 +0200

    Update commands.py

--- tgbot/handlers/commands.py
+++ tgbot/handlers/commands.py
@@ -10,7 +10,8 @@ from tgbot.sheets.take_from_sheet import (
     get_klaar_data,
     get_kris_data,
     get_welcome,
-    convert_drive_url
+    convert_drive_url,
+    fetch_participants
 )
 
 router = Router()
@@ -97,3 +98,44 @@ async def send_chat_id(message: Message):
     chat_id = message.chat.id
     await message.answer(f"Ваш Chat ID: `{chat_id}`", parse_mode="Markdown")
     logging.info(f"Chat ID ({chat_id}) отправлен пользователю {message.from_user.id}")
+
+@router.message(Command(commands=["kto"]))
+async def who_is_this(message: Message):
+    participants = fetch_participants()
+    if not participants:
+        await message.answer("Ошибка загрузки данных из Google Sheets.")
+        return
+
+    args = message.text.split(' ', 1)
+
+    if len(args) < 2:
+        await message.answer("Пожалуйста, укажите имя после команды или 'all' для всех.")
+        return
+
+    name = args[1].strip().lower()
+
+    if name == "all":
+        response = "Список всех пользователей:\n"
+        for user_name, user_info in participants.items():
+            # Показываем только уникальные записи по основному имени
+            if user_name == user_info["name"].lower():
+                response += (
+                    f"\nИмя: {user_info['name']}\n"
+                    f"{f'Имя в телеграмм: {user_info['tgnick']}\n' if user_info['tgnick'] != 'Unknown' else ''}"
+                    f"{f'Ник: @{user_info['nick']}\n' if user_info['nick'] != 'Unknown' else ''}"
+                    f"Инфо: {user_info['about']}\n"
+                )
+        await message.answer(response)
+    else:
+        user_info = participants.get(name)
+        if user_info:
+            response = (
+                f"Имя: {user_info['name']}\n"
+                f"{f'Имя в телеграмм: {user_info['tgnick']}\n' if user_info['tgnick'] != 'Unknown' else ''}"
+                f"{f'Ник: @{user_info['nick']}\n' if user_info['nick'] != 'Unknown' else ''}"
+                f"Инфо: {user_info['about']}"
+            )
+            await message.answer(response)
+        else:
+            await message.answer(f"Информация о пользователе '{args[1]}' не найдена.")
+
