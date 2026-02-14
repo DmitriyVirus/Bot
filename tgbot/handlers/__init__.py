1074d8faa27b838ca84e85e439287e36dc386477
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-14 20:22:27 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,15 +1,12 @@
 # Импортируем все роутеры из отдельных файлов
 from .menu import router as menu_router
-from .kto import router as kto_router
 from .commands import router as commands_router
 from .greetings import router as greetings_router
 from .sbor import router as sbor_router
-from .kto import fetch_data_from_sheet
 from aiogram import Router
 
 # Создаём единый роутер для всех хэндлеров
 router = Router()
-router.include_router(kto_router)
 router.include_router(menu_router)
 router.include_router(sbor_router)
 router.include_router(commands_router)
