caef0b8be89117ba49ea871121b57fd10faf79bd
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-14 20:10:54 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,7 +1,7 @@
 # Импортируем все роутеры из отдельных файлов
 from .menu import router as menu_router
 from .kto import router as kto_router
-from .triggers import router as triggers_router
+from .commands import router as commands_router
 from .greetings import router as greetings_router
 from .sbor import router as sbor_router
 from .kto import fetch_data_from_sheet
@@ -12,5 +12,5 @@ router = Router()
 router.include_router(kto_router)
 router.include_router(menu_router)
 router.include_router(sbor_router)
-router.include_router(triggers_router)
+router.include_router(commands_router)
 router.include_router(greetings_router)
