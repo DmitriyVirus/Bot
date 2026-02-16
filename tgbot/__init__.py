99c59310b53a8fdbcc0d22cc828294241898a28a
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-16 21:34:58 +0200

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -3,11 +3,13 @@ import asyncio
 from aiogram import Bot, Dispatcher, Router
 from tgbot.handlers import router as handlers_router
 from tgbot.sheets import router as sheets_router
+from tgbot.redis import router as redis_router
+
 
 router = Router()
 router.include_router(handlers_router)
 router.include_router(sheets_router)
-
+router.include_router(redis_router)
 
 class TGBot:
     def __init__(self, router: Router) -> None:
@@ -34,3 +36,4 @@ class TGBot:
 
 
 tgbot = TGBot(router)
+
