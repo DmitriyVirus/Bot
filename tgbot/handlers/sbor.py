51f74967029d832771d93e052479ad206eb8d61a
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-02-17 18:03:02 +0200

    Update sbor.py

--- tgbot/handlers/sbor.py
+++ tgbot/handlers/sbor.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import types, Router
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from tgbot.sheets.take_from_sheet import get_column_data_from_autosbor, get_name_username_dict
-from tgbot.redis.redis_cash import get_name, get_allowed_user_ids, get_bal_data, get_inn_data, get_ork_data, get_inst_data
+from tgbot.sheets.take_from_sheet import get_column_data_from_autosbor
+from tgbot.redis.redis_cash import get_name, get_allowed_user_ids, get_bal_data, get_inn_data, get_ork_data, get_inst_data, get_name_username_dict
 
 logging.basicConfig(level=logging.DEBUG)
 router = Router()
