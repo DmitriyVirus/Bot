3411583f37e34973e03b1a4ccfc3cb9b5dd87c52
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-03-06 17:08:11 +0200

    Update redis_cash.py

--- tgbot/redis/redis_cash.py
+++ tgbot/redis/redis_cash.py
@@ -273,7 +273,7 @@ def load_all_data_to_redis():
     pipe_all.delete(REDIS_KEY_ALL_DATA)
 
     # Events
-    events_map = {"bal": ("J2","J3"), "inn":("J5","J6"), "ork":("J8","J9"), "inst":("J11","J12"), "freya":("J14","J15"), "ramona":("J17","J18"), "bless": ("J20","J21")}
+    events_map = {"bal": ("J2","J3"), "inn":("J5","J6"), "ork":("J8","J9"), "inst":("J11","J12"), "freya":("J14","J15"), "ramona":("J17","J18"), "bless": ("J21","J20")}
     for event, (text_cell, media_cell) in events_map.items():
         text = sheet_info.acell(text_cell).value or ""
         media_url = convert_drive_url(sheet_info.acell(media_cell).value or "")
