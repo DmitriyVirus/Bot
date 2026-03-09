0bee34f51b20d3cc0646fc1342fa51282f27ae1b
DmitriyVirus <dmitriy.ma11igos@gmail.com>
2026-03-09 14:09:16 +0200

    Update __init__.py

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from .menu import router as menu_router
 from .sbor import router as sbor_router
+from .save import router as save_router
 from .bless import router as bless_router
 from .commands import router as commands_router
 from .greetings import router as greetings_router
@@ -9,6 +10,7 @@ from .greetings import router as greetings_router
 
 router = Router()
 router.include_router(menu_router)
+router.include_router(save_router)
 router.include_router(sbor_router)
 router.include_router(bless_router)
 router.include_router(commands_router)
